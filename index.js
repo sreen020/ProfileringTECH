d07d86966b9fc2b0946a481c6dfc76ff761637b2
sreen020 <sjoerdreen@gmail.com>
2020-05-14 13:47:17 +0200

    Homework week 2

--- index.js
+++ index.js
@@ -2,6 +2,46 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+const handlebars = require('express-handlebars');
+
+app.set('view engine', 'hbs');
+app.engine('hbs', handlebars({
+    layoutsDir: __dirname + '/views/layouts',
+    extname: 'hbs',
+    defaultLayout: 'planB',
+    partialsDir: __dirname + '/views/layouts/partials/',
+}));
+
+app.use(express.static('public'))
+
+
+fakeApi = () => {
+    return [
+      {
+        name: 'Katarina',
+        lane: 'midlaner'
+      },
+      {
+        name: 'Jayce',
+        lane: 'toplaner'
+      },
+      {
+        name: 'Heimerdinger',
+        lane: 'toplaner'
+      },
+      {
+        name: 'Zed',
+        lane: 'midlaner'
+      },
+      {
+        name: 'Azir',
+        lane: 'midlaner'
+      }
+    ];
+  }
+
+  const list = true;
+
 app.get('/', home)
 app.get('/profile', profile)
 
@@ -10,9 +50,12 @@ app.listen(port, function() {
 });
 
 function home(req, res) {
-    res.send("hello world");
-}
+    res.render('main', {layout: 'index', suggestedChamps: fakeApi(), listExists: true});
+};
 
 function profile(req, res) {
-    res.send("hello GANGGGGG");
+    res.send("hello");
 }
+
+
+
